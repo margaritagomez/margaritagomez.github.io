29b6a3884d5d1d0052d1676cf7c3c151251681db
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-02-10 10:36:14 -0500

    Contact Me section almost ready

---
+++ js/contact.js
@@ -0,0 +1,42 @@
+
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+
+function animationHoverShow(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            $('#plis').css('visibility','visible');
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+
+$(document).ready(function(){
+    $('.ho').each(function() {
+        animationHover(this, 'pulse');
+    });
+});
+
+$(document).ready(function(){
+    $('#plis').css('visibility','hidden');
+    $('#contact-me').each(function() {
+        animationHoverShow(this, 'fadeInDown');
+    });
+});
